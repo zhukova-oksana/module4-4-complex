ebef2373c3e6a0cf645f9b66ee3b0354686e82a0
zhukova_oksana <ksy2905@gmail.com>
2022-08-09 21:08:45 +0300

    edit task02 formule

--- js/hard_task_02.js
+++ js/hard_task_02.js
@@ -5,7 +5,9 @@ const num2 = +prompt('Введите второе число');
 
 const getMinNumber = (num1, num2) => {
   // let minNumber = (num1 < num2) ? num1 : num2;
-  let minNumber = ((num1 + num2) - Math.abs(num1 - num2)) / 2;
+  // let minNumber = ((num1 + num2) - Math.abs(num1 - num2)) / 2;
+  
+  let minNumber = (num1 > num2) * num2 + (num2 > num1) * num1;
   return minNumber;
 }
 
